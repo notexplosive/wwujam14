76091eeb80c7b552c3074eeea8f603246c1af979
Cullen <owenc4@wwu.edu>
2020-03-07 16:47:00 -0800

    Movement 2

--- wwu/movement.lua
+++ wwu/movement.lua
@@ -22,7 +22,33 @@ function Movement:update(dt)
 
 	inputVector = inputVector:normalized() * self.moveSpeed * dt
 
+	local room = self:getCurrentRoom()
+	local rect = self.actor.BoundingBox:getRect()
+	rect:move(inputVector)
+
+	if rect:left() < room:pos().x then
+		inputVector = Vector.new()
+	end
+
+	if rect:right() > room:pos().x + room.BoundingBox:width() then
+		inputVector = Vector.new()
+	end
+
 	self.actor:move(inputVector)
+end
+
+function Movement:getCurrentRoom()
+	local myRect = self.actor.BoundingBox:getRect()
+
+	for i, roomActor in self.actor:scene():eachActorWith(Components.Room) do
+		local roomBounds = roomActor.BoundingBox:getRect()
+
+		if myRect:getIntersection(roomBounds):area() > 0 then
+			return roomActor
+		end
+	end
+
+	assert(false, "you are not in a room :(")
 end
 
 return Movement
---
+++ wwu/room.lua
@@ -0,0 +1,5 @@
+local Room = {}
+
+registerComponent(Room, "Room")
+
+return Room
