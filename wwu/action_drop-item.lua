4bcebc629dd2935e6d1326d97fba726b52ef55b3
Wyatt Chapman <thenotexplosive@gmail.com>
2020-03-08 08:22:20 -0700

    REFACTOR - rename; prepped DropItemInRoom

---
+++ wwu/action_drop-item.lua
@@ -0,0 +1,32 @@
+local DropItemInRoom = {}
+
+registerComponent(DropItemInRoom, "DropItemInRoom", {"CanTraverseDoors", "Inventory"})
+
+function DropItemInRoom:setup()
+end
+
+function DropItemInRoom:awake()
+end
+
+function DropItemInRoom:update(dt)
+end
+
+function DropItemInRoom:isOverlappingDoor()
+    return self.actor.CanTraverseDoors:getCurrentDoor() ~= nil
+end
+
+function DropItemInRoom:getDirection()
+    assert(self.actor.Inventory:isHolding(), self.actor.name .. " had action DropItemInRoom, was not holding an item")
+
+    return 0
+end
+
+function DropItemInRoom:isAble()
+    return self.actor.Inventory:isHolding() and not self:isOverlappingDoor()
+end
+
+function DropItemInRoom:isFinished()
+    return not self.actor.Inventory:isHolding()
+end
+
+return DropItemInRoom
