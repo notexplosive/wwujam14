b57496e2f3aeac35a4c74df1c36599e6b579d04c
Wyatt Chapman <thenotexplosive@gmail.com>
2020-03-08 09:25:47 -0700

    Delay after an action is completed

--- wwu/npc-input.lua
+++ wwu/npc-input.lua
@@ -5,22 +5,28 @@ registerComponent(NpcInput, "NpcInput")
 function NpcInput:setup()
 end
 
-function NpcInput:getSpeed()
-	return 150
-end
-
 function NpcInput:awake()
 	self.inputState = {left = false, right = false}
 	self.interactTimer = 0
+	self.waitTimer = 0
 end
 
 function NpcInput:update(dt)
+	if self.waitTimer > 0 then
+		self.waitTimer = self.waitTimer - dt
+		return
+	end
+
 	if self.actor.Plan then
 		local pendingComponent = self.actor.Plan:getPendingComponent()
 		if pendingComponent then
 			self:calculateInputFromAction(pendingComponent, dt)
 		end
 	end
+end
+
+function NpcInput:getSpeed()
+	return 150
 end
 
 -- actions need:
@@ -45,9 +51,10 @@ end
 
 function NpcInput:attemptInteract(dt)
 	self.interactTimer = self.interactTimer + dt
-	if self.interactTimer > 0.25 then
+	if self.interactTimer > 0.5 then
 		self.actor.CanInteract:interact()
 		self.interactTimer = 0
+		self.waitTimer = 0.5
 	end
 end
 
